fd44e1651d8743cf00b73ccb4bb13e35d255986d
Arash Tadjiki <atadjiki@Deep-Space-Nine.local>
2022-01-02 15:08:52 -0600

    approach and interact

--- Dealer_Prototype/Assets/Scripts/Behaviors/Behavior_Sit.cs
+++ Dealer_Prototype/Assets/Scripts/Behaviors/Behavior_Sit.cs
@@ -14,26 +14,7 @@ public class Behavior_Sit : CharacterBehaviorScript
 
     protected override IEnumerator Behavior()
     {
-        _data.Character.SetUpdateState(CharacterConstants.UpdateState.Busy);
-
-        Chair chair = (Chair)_data.Interactable;
-
-        if(chair != null)
-        {
-            Transform sittingTransform = chair.GetSittingPoseTransform();
-
-            //move NPC to interaction location
-            _data.Character.GetNavigatorComponent().transform.position = sittingTransform.position;
-            _data.Character.GetNavigatorComponent().transform.rotation = sittingTransform.rotation;
-
-            DebugManager.Instance.Print(DebugManager.Log.LogBehavior, _data.Character.GetID() + " teleporting to  " + _data.Interactable.GetID());
-
-            _data.Character.FadeToAnimation(AnimationConstants.Animations.Sitting_Idle, 0.05f, false);
-
-            DebugManager.Instance.Print(DebugManager.Log.LogBehavior, _data.Character.GetID() + " sitting on " + _data.Interactable.GetID());
-
-            _data.Character.SetUpdateState(CharacterConstants.UpdateState.Ready);
-        }
+        yield return BehaviorHelper.PerformSit(_data);
 
         yield return base.Behavior();
     }
