3fcb6c4e072b1e324aca94bd300b06fbf1d9841a
Arash Tadjiki <atadjiki@Deep-Space-Nine.local>
2022-10-07 20:54:03 -0500

    character canvas coming along

--- Dealer_Prototype/Assets/Scripts/CharacterData/CharacterInfo.cs
+++ Dealer_Prototype/Assets/Scripts/CharacterData/CharacterInfo.cs
@@ -4,9 +4,13 @@ using Constants;
 using UnityEngine;
 
 [System.Serializable]
-public class CharacterInfo : MonoBehaviour
+public class CharacterInfo 
 {
+    [Header("General Data")]
     public string CharacterName;
+    public int Health;
+    public int Level;
+
     public Enumerations.WeaponID WeaponID;
     public Enumerations.CharacterModelID CharacterModelID;
 
