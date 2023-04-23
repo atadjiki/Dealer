cbb1ab8fecfb0bde80183da018683d176b2fb2b0
Arash Tadjiki <atadjiki@Deep-Space-Nine.local>
2023-04-23 13:37:18 -0500

    working on stash panel

--- Dealer_Mobile/Assets/Scripts/UI/Safehouse/Stash/OverlayMenu_Stash.cs
+++ Dealer_Mobile/Assets/Scripts/UI/Safehouse/Stash/OverlayMenu_Stash.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class OverlayMenu_Stash : OverlayMenu
 {
-    
+    [SerializeField] private TextMeshProUGUI Text_Capacity;
+
+    [Header("Header")]
+    [SerializeField] private Button Button_Header_Name;
+    [SerializeField] private Button Button_Header_Quality;
+    [SerializeField] private Button Button_Header_Quantity;
+
+    [Header("Detail Panel")]
+    [SerializeField] private Image Image_Item;
+    [SerializeField] private TextMeshProUGUI Text_Item;
+
+    [Header("Prefab")]
+    [SerializeField] private GameObject Prefab_ListItem;
 }
