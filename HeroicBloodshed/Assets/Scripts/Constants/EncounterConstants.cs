6d300ec4a750fb9288a283053e5b5e9203b4bbb2
Arash Tadjiki <atadjiki@Deep-Space-Nine.local>
2023-08-30 16:52:12 -0500

    figuring out encounter state loop

--- HeroicBloodshed/Assets/Scripts/Constants/EncounterConstants.cs
+++ HeroicBloodshed/Assets/Scripts/Constants/EncounterConstants.cs
@@ -1,6 +1,27 @@
 public static partial class Constants
 {
-    public enum EncounterStateTag
+    public enum EncounterState
     {
+        SETUP,//waiting for encounter to begin
+
+        BUILD_QUEUES,//build queues to see which characters have actions this turn
+
+        CHECK_CONDITIONS,//check queues to see if the encounter can be declared Done
+
+        DONE,//the encounter is done and we can exit
+
+        PROCESS_TURN,//see which team queue to process
+         
+        SELECT_CURRENT_CHARACTER,//select the next character in the current team queue
+
+        WAIT_FOR_PLAYER_INPUT,//if player turn, wait for input
+
+        CHOOSE_AI_ACTION,//if AIs turn, choose an action for them 
+
+        PERFORM_ACTION,//perform the chosen action
+
+        DESELECT_CURRENT_CHARACTER,//deselect current character and pop them from their team's queue
+
+        UPDATE //update the turn count, etc 
     }
 }
--- HeroicBloodshed/Assets/Scripts/Encounter/Encounter.cs
+++ HeroicBloodshed/Assets/Scripts/Encounter/Encounter.cs
@@ -9,6 +9,15 @@ using static Constants;
 [Serializable]
 public class Encounter : MonoBehaviour
 {
+    //event handling
+    public delegate void EncounterStateDelegate(EncounterState encounterState);
+    public EncounterStateDelegate OnStateChanged;
+
+    //setup data
+    EncounterSetupData _setupData;
+
+    public void SetSetupData(EncounterSetupData setupData) { _setupData = setupData; }
+
     //collections
     //when setup is performed, store spawned characters
     private Dictionary<TeamID, List<CharacterComponent>> _characterMap;
@@ -16,7 +25,23 @@ public class Encounter : MonoBehaviour
     //create a queue for each team each combat loop
     private Dictionary<TeamID, Queue<CharacterComponent>> _queues;
 
-    private HashSet<EncounterStateTag> _tagContainer = new HashSet<EncounterStateTag>();
+    private EncounterState _pendingState = EncounterState.SETUP;
+
+    public EncounterState GetState() { return _pendingState; }
+
+    private bool _busy = false;
+
+    private void SetState(EncounterState state)
+    {
+        Debug.Log("Transitioning from " + _pendingState + " to " + state);
+
+        _pendingState = state;
+
+        if (OnStateChanged != null)
+        {
+            OnStateChanged.Invoke(_pendingState);
+        }
+    }
 
     private int _turnCount;
 
@@ -26,25 +51,79 @@ public class Encounter : MonoBehaviour
 
     private Transform _defaultCameraFollow;
 
-    public void Setup(EncounterSetupData setupData)
+    public void TransitionState()
     {
-        StartCoroutine(Coroutine_Setup(setupData));
+        if(!_busy)
+        {
+            StartCoroutine(Coroutine_TransitionState());
+        }
+        else
+        {
+            Debug.Log("Encounter is busy");
+        }
+
     }
 
-    private IEnumerator Coroutine_Setup(EncounterSetupData setupData)
+    private IEnumerator Coroutine_TransitionState()
     {
-        Debug.Log("Setting up encounter " + setupData.name);
+        _busy = true;
 
-        _virtualCamera = setupData.VirtualCamera;
+        Debug.Log("Handling state " + _pendingState);
+
+        switch(_pendingState)
+        {
+            case EncounterState.SETUP:
+                yield return Coroutine_Setup();
+                break;
+            case EncounterState.BUILD_QUEUES:
+                yield return Coroutine_BuildQueues();
+                break;
+            case EncounterState.CHECK_CONDITIONS:
+                yield return Coroutine_CheckConditions();
+                break;
+            case EncounterState.PROCESS_TURN:
+                yield return Coroutine_ProcessTurn();
+                break;
+            case EncounterState.SELECT_CURRENT_CHARACTER:
+                yield return Coroutine_SelectCurrentCharacter();
+                break;
+            case EncounterState.WAIT_FOR_PLAYER_INPUT:
+                yield return Coroutine_WaitForInput();
+                break;
+            case EncounterState.CHOOSE_AI_ACTION:
+                yield return Coroutine_ChooseAIAction();
+                break;
+            case EncounterState.PERFORM_ACTION:
+                yield return Coroutine_PerformAction();
+                break;
+            case EncounterState.DESELECT_CURRENT_CHARACTER:
+                yield return Coroutine_DeselectCurrentCharacter();
+                break;
+            case EncounterState.UPDATE:
+                yield return Coroutine_UpdateEncounter();
+                break;
+            default:
+                Debug.Log("No state transition available!");
+                break;
+        }
+
+        _busy = false;
+
+        yield return null;
+    }
+
+    //state behavior
+    private IEnumerator Coroutine_Setup()
+    {
+        _virtualCamera = _setupData.VirtualCamera;
         _defaultCameraFollow = _virtualCamera.Follow;
 
-        // _stateID = EncounterStateID.NONE;
         _turnCount = 0;
         _currentTeam = TeamID.Player; //the player always goes first
 
         _characterMap = new Dictionary<TeamID, List<CharacterComponent>>();
 
-        foreach (EncounterTeamData teamData in setupData.Teams)
+        foreach (EncounterTeamData teamData in _setupData.Teams)
         {
             _characterMap.Add(teamData.Team, new List<CharacterComponent>());
 
@@ -71,11 +150,10 @@ public class Encounter : MonoBehaviour
 
         yield return EncounterHelper.SpawnCharacters(this);
 
-        AddTag(EncounterStateTag.WAITING_FOR_INPUT);
+        SetState(EncounterState.BUILD_QUEUES);
     }
 
-    //this is called at the beginning of every turn
-    public void BuildTeamQueues()
+    private IEnumerator Coroutine_BuildQueues()
     {
         _queues = new Dictionary<TeamID, Queue<CharacterComponent>>();
 
@@ -94,10 +172,13 @@ public class Encounter : MonoBehaviour
                 }
             }
         }
+
+        SetState(EncounterState.CHECK_CONDITIONS);
+
+        yield return null;
     }
 
-    //check queues and see if we have met the conditions to consider the encounter done
-    public void CheckWinConditions()
+    private IEnumerator Coroutine_CheckConditions()
     {
         //if any queues are empty, then we are done with the encounter
         foreach (Queue<CharacterComponent> TeamQueue in _queues.Values)
@@ -105,9 +186,60 @@ public class Encounter : MonoBehaviour
             if (TeamQueue.Count == 0)
             {
                 //call delegate
-                return;
+                SetState(EncounterState.DONE);
+                yield break;
             }
         }
+
+        SetState(EncounterState.PROCESS_TURN);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_Done()
+    {
+        SetState(EncounterState.DONE);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_ProcessTurn()
+    {
+        SetState(EncounterState.SELECT_CURRENT_CHARACTER);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_SelectCurrentCharacter()
+    {
+        SetState(EncounterState.CHOOSE_AI_ACTION);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_WaitForInput()
+    {
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_ChooseAIAction()
+    {
+        SetState(EncounterState.PERFORM_ACTION);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_PerformAction()
+    {
+        SetState(EncounterState.DESELECT_CURRENT_CHARACTER);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_DeselectCurrentCharacter()
+    {
+        SetState(EncounterState.UPDATE);
+        yield return null;
+    }
+
+    private IEnumerator Coroutine_UpdateEncounter()
+    {
+        SetState(EncounterState.BUILD_QUEUES);
+        yield return null;
     }
 
     //getters/setters
@@ -122,7 +254,7 @@ public class Encounter : MonoBehaviour
     {
         List<CharacterComponent> result = new List<CharacterComponent>();
 
-        foreach(List<CharacterComponent> teamList in _characterMap.Values)
+        foreach (List<CharacterComponent> teamList in _characterMap.Values)
         {
             result.AddRange(teamList);
         }
@@ -207,24 +339,5 @@ public class Encounter : MonoBehaviour
     public void ResetCameraFollow()
     {
         SetCameraFollow(_defaultCameraFollow);
-    }
-
-
-    //state
-    public void AddTag(EncounterStateTag tag)
-    {
-        Debug.Log("Adding tag " + tag);
-        _tagContainer.Add(tag);
-    }
-
-    public void RemoveTag(EncounterStateTag tag)
-    {
-        Debug.Log("Removing tag " + tag);
-        _tagContainer.Remove(tag);
-    }
-
-    public bool HasTag(EncounterStateTag tag)
-    {
-        return _tagContainer.Contains(tag);
     }
 }
