76e4fd51cdebe565b20463d705144eeef33990c5
Arash Tadjiki <atadjiki@Deep-Space-Nine.local>
2023-09-25 12:39:41 -0500

    adding muzzle lights

--- HeroicBloodshed/Assets/Scripts/Character/Base/CharacterDecal.cs
+++ HeroicBloodshed/Assets/Scripts/Character/Base/CharacterDecal.cs
@@ -2,21 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using static Constants;
 using UnityEngine;
+using EPOOutline;
 
 public class CharacterDecal : MonoBehaviour
 {
+    [SerializeField] private Outlinable Outline;
+
     public void SetColor(TeamID team)
     {
-        Color teamColor = GetColorByTeam(team, 0.25f);
+        Color teamColor = GetColorByTeam(team, 1.0f);
 
-        Renderer renderer = this.GetComponent<Renderer>();
+        Outline.OutlineParameters.Color = teamColor;
 
-        if(renderer != null)
-        {
-            if(renderer.material != null)
-            {
-                renderer.material.color = teamColor;
-            }
-        }
+        teamColor.a = 0.15f;
+
+        Outline.OutlineParameters.FillPass.SetColor("_PublicColor", teamColor);
     }
 }
