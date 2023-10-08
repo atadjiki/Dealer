97f6d661f4a695d83e7c2c17be883e620659bc5a
Arash Tadjiki <atadjiki@Deep-Space-Nine.local>
2023-10-08 17:12:20 -0500

    progress on generating grid

--- HeroicBloodshed/Assets/Scripts/Encounter/EncounterGridVisualizer.cs
+++ HeroicBloodshed/Assets/Scripts/Encounter/EncounterGridVisualizer.cs
@@ -2,28 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEditor;
 
 public class EncounterGridVisualizer : MonoBehaviour
 {
     [SerializeField] private GameObject TilePrefab;
 
-    private void Awake()
+    [SerializeField] private int Rows = 8;
+
+    [SerializeField] private int Columns = 8;
+
+    public void GenerateTiles()
     {
         GridGraph gridGraph = AstarPath.active.data.gridGraph;
-        int count = 1;
+        int index = 0;
 
         gridGraph.GetNodes(node =>
         {
-            Debug.Log("Node " + count + " " + node.position.ToString());
-  
+            Debug.Log("Node " + index + " " + node.position.ToString());
+
+            int col = (index % Columns);
+            int row = (index / Columns);
+
             Vector3 pos = ((Vector3)node.position);
 
             GameObject tileDecal = Instantiate<GameObject>(TilePrefab, pos, Quaternion.identity, this.transform);
-            tileDecal.name = "Node " + count;
+            tileDecal.name = "Node " + index + " [ " + row + "," + col + " ] ";
 
-            count++;
+            index++;
 
             return true;
         });
+    }
+}
+
+[CustomEditor(typeof(EncounterGridVisualizer))]
+public class EncounterTileBuilder : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Generate Tiles"))
+        {
+            EncounterGridVisualizer gridVisualizer = (EncounterGridVisualizer)target;
+            gridVisualizer.GenerateTiles();
+        }
+
+        if(GUILayout.Button("Clear"))
+        {
+            EncounterGridVisualizer gridVisualizer = (EncounterGridVisualizer)target;
+
+            for (int i = 0; i < gridVisualizer.transform.childCount; i++)
+            {
+                DestroyImmediate(gridVisualizer.transform.GetChild(i).gameObject);
+            }
+        }
     }
 }
