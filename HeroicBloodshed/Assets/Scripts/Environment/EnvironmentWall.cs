13c06c6e9e7cda43bc815d9e3bc0d220097d2ae4
Arash Tadjiki <atadjiki@gmail.com>
2023-11-08 22:33:15 -0600

    working on outlines

--- HeroicBloodshed/Assets/Scripts/Environment/EnvironmentWall.cs
+++ HeroicBloodshed/Assets/Scripts/Environment/EnvironmentWall.cs
@@ -5,79 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class EnvironmentWall : MonoBehaviour
 {
-    [SerializeField] protected GameObject Mesh_Default;
-    [SerializeField] protected GameObject Mesh_Alternate;
+    private List<MeshRenderer> _renderers;
 
-    [SerializeField] private bool CheckOverlap = true;
-
-    private BoxCollider _collider;
+    private float fadeDistance = 10; //if the camera is farther away than this, opacity is 100%
 
     private void Awake()
     {
-        _collider = GetComponent<BoxCollider>();   
+        _renderers = new List<MeshRenderer>(GetComponentsInChildren<MeshRenderer>());
     }
+
     private void Update()
     {
-        if (CheckOverlap_Mouse()) return;
-        CheckOverlap_Camera();
-    }
-    private bool CheckOverlap_Mouse()
-    {
-        if (CheckOverlap == false) return false;
-        if (Camera.main == null) return false;
-        if (_collider == null) return false;
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, LayerMask.GetMask("EnvironmentWall")))
-        {
-            if (hit.collider != null && hit.collider == _collider)
-            {
-                ToggleVisibility(false);
-                return true;
-            }
-        }
-
-        ToggleVisibility(true);
-        return false;
-    }
-
-    private void CheckOverlap_Camera()
-    {
-        if (CheckOverlap == false) return;
         if (Camera.main == null) return;
-        if (_collider == null) return;
 
-        Vector3 origin = Camera.main.transform.position;
-        Vector3 direction = Camera.main.transform.forward;
+        Vector3 cameraPos = Camera.main.transform.position;
 
-        Ray ray = new Ray(origin, direction);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, LayerMask.GetMask("EnvironmentWall")))
+        float distanceToCamera = Vector3.Distance(cameraPos, this.transform.position);
+
+        if(distanceToCamera < fadeDistance)
         {
-            if (hit.collider != null && hit.collider == _collider)
+            float fadePercentage = distanceToCamera / fadeDistance;
+
+            foreach(MeshRenderer renderer in _renderers)
             {
-                ToggleVisibility(false);
-                return;
+                if(renderer.material != null)
+                {
+                    Color color = renderer.material.color;
+
+                    color.a = fadePercentage;
+
+                    renderer.material.color = color;
+                }
             }
         }
-
-        ToggleVisibility(true);
     }
-
-    private void ToggleVisibility(bool flag)
-    {
-        if (flag)
-        {
-            Mesh_Default.SetActive(true);
-            Mesh_Alternate.SetActive(false);
-        }
-        else
-        {
-            Mesh_Default.SetActive(false);
-            Mesh_Alternate.SetActive(true);
-        }
-    }
-
-
 }
