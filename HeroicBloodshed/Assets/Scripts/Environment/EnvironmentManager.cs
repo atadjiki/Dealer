78d4e7a154868cf63f8bf280917ef716b3ca7364
Arash Tadjiki <atadjiki@gmail.com>
2023-11-14 15:40:40 -0600

    progress on untangling environemnt tiles

--- HeroicBloodshed/Assets/Scripts/Environment/EnvironmentManager.cs
+++ HeroicBloodshed/Assets/Scripts/Environment/EnvironmentManager.cs
@@ -11,10 +11,11 @@ public class EnvironmentManager: MonoBehaviour, IEncounterEventHandler
     private static EnvironmentManager _instance;
     public static EnvironmentManager Instance { get { return _instance; } }
 
-    private EnvironmentTileGrid _tileGrid;
+    private List<EnvironmentSpawnPoint> _spawnPoints;
+
+    private List<EnvironmentObstacle> _obstacles;
 
     //Setup
-
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -30,7 +31,7 @@ public class EnvironmentManager: MonoBehaviour, IEncounterEventHandler
     public IEnumerator Coroutine_Build()
     {
         yield return Coroutine_ScanNavmesh();
-        yield return Coroutine_BuildTileGrid();
+        yield return Coroutine_GatherEnvironmentObjects();
 
         //dispose of the setup navmesh after tiles are built
         GridGraph gridGraph = AstarPath.active.data.gridGraph;
@@ -41,41 +42,143 @@ public class EnvironmentManager: MonoBehaviour, IEncounterEventHandler
 
     private IEnumerator Coroutine_ScanNavmesh()
     {
-        Debug.Log("Scanning navmesh");
+        Debug.Log("Scanning NavMesh");
         GridGraph gridGraph = AstarPath.active.data.gridGraph;
         AstarPath.active.Scan(gridGraph);
 
         yield return new WaitWhile(() => AstarPath.active.isScanning);
     }
 
-    private IEnumerator Coroutine_BuildTileGrid()
+    private IEnumerator Coroutine_GatherEnvironmentObjects()
     {
-        Debug.Log("Building tile grid");
-        _tileGrid = GetComponentInChildren<EnvironmentTileGrid>();
-        yield return _tileGrid.Corutine_PerformSetup();
+        Debug.Log("Gathering Environment Objects...");
+
+        _spawnPoints = new List<EnvironmentSpawnPoint>();
+        _obstacles = new List<EnvironmentObstacle>();
+
+        foreach(EnvironmentSpawnPoint spawnPoint in GetComponentsInChildren<EnvironmentSpawnPoint>())
+        {
+            Vector3 position = spawnPoint.transform.position;
+
+            Vector3 result;
+            if(GetClosestNodeToPosition(position, out result))
+            {
+                _spawnPoints.Add(spawnPoint);
+            }
+            else
+            {
+                Debug.Log("Failed to gather spawn point " + spawnPoint.name);
+                Destroy(spawnPoint.gameObject);
+            }
+        }
+
+        foreach (EnvironmentObstacle obstacle in GetComponentsInChildren<EnvironmentObstacle>())
+        {
+            Vector3 position = obstacle.transform.position;
+
+            Vector3 result;
+            if (GetClosestNodeToPosition(position, out result))
+            {
+                _obstacles.Add(obstacle);
+            }
+            else
+            {
+                Debug.Log("Failed to gather obstacle " + obstacle.name);
+                Destroy(obstacle.gameObject);
+            }
+        }
+
+        yield return null;
     }
 
     //Encounter Events
 
     public IEnumerator Coroutine_EncounterStateUpdate(EncounterState stateID, EncounterModel model)
     {
-        if (_tileGrid != null)
-        {
-            yield return _tileGrid.Coroutine_EncounterStateUpdate(stateID, model);
-        }
+        //if (_tileGrid != null)
+        //{
+        //    yield return _tileGrid.Coroutine_EncounterStateUpdate(stateID, model);
+        //}
 
         yield return null;
     }
 
     //Helpers/interface 
-
-    public List<EnvironmentTile> GetTilesContainingSpawnPoints(TeamID teamID)
+    public bool GetClosestNodeToPosition(Vector3 position, out Vector3 result)
     {
-        if (_tileGrid != null)
+        GridGraph gridGraph = AstarPath.active.data.gridGraph;
+
+        NNInfoInternal nnInfo = gridGraph.GetNearest(position);
+
+        if (nnInfo.node != null)
         {
-            return _tileGrid.GetTilesContainingSpawnPoints(teamID);
+            result = nnInfo.clampedPosition;
+            return true;
+        }
+        else
+        {
+            result = Vector3.zero;
+            return false;
+        }
+    }
+    public CharacterComponent SpawnCharacter(TeamID teamID, CharacterID characterID)
+    {
+        //find a spawn point to place the character
+        //see if we have a marker available to spawn them in
+        foreach (EnvironmentSpawnPoint spawnPoint in _spawnPoints)
+        {
+            Vector3 location;
+            if (GetClosestNodeToPosition(spawnPoint.transform.position, out location))
+            {
+                if (spawnPoint.GetTeam() == teamID)
+                {
+                    GameObject characterObject = new GameObject(teamID + "_" + characterID);
+
+                    CharacterComponent characterComponent = AddComponentByTeam(characterID, characterObject);
+
+                    characterObject.transform.parent = this.transform;
+                    characterObject.transform.localPosition = Vector3.zero;
+                    characterObject.transform.localEulerAngles = spawnPoint.transform.eulerAngles;
+                    characterObject.transform.position = location;
+
+                    return characterComponent;
+                }
+            }
+           else
+            {
+                Debug.Log("Couldnt find location to spawn character");
+            }
         }
 
         return null;
+    }
+
+    private static CharacterComponent AddComponentByTeam(CharacterID characterID, GameObject characterObject)
+    {
+        TeamID teamID = GetTeamByID(characterID);
+
+        switch (teamID)
+        {
+            case TeamID.Player:
+                PlayerCharacterComponent playerCharacterComponent = characterObject.AddComponent<PlayerCharacterComponent>();
+                playerCharacterComponent.SetID(characterID);
+                return playerCharacterComponent;
+            case TeamID.Enemy:
+                EnemyCharacterComponent enemyCharacterComponent = characterObject.AddComponent<EnemyCharacterComponent>();
+                enemyCharacterComponent.SetID(characterID);
+                return enemyCharacterComponent;
+            default:
+                return null;
+        }
+    }
+
+    public List<EnvironmentSpawnPoint> GetSpawnPoints()
+    {
+        return _spawnPoints;
+    }
+
+    public List<EnvironmentObstacle> GetObstacles()
+    {
+        return _obstacles;
     }
 }
--- HeroicBloodshed/Assets/Scripts/Environment/EnvironmentUtil.cs
+++ HeroicBloodshed/Assets/Scripts/Environment/EnvironmentUtil.cs
@@ -5,60 +5,6 @@ using static Constants;
 
 public class EnvironmentUtil : MonoBehaviour
 {
-    public static CharacterComponent SpawnCharacter(TeamID teamID, CharacterID characterID)
-    {
-        if(EnvironmentManager.Instance == null)
-        {
-            Debug.Log("Environment Manager is not initialized");
-                return null;
-        }
 
-        //find a spawn point to place the character
-        ////see if we have a marker available to spawn them in
-        //foreach (EnvironmentTile tile in EnvironmentManager.Instance.GetTilesContainingSpawnPoints(teamID))
-        //{
-        //    GameObject characterObject = CreateCharacterObject(teamID + "_" + characterID, tile);
-        //    CharacterComponent characterComponent = AddComponentByTeam(characterID, characterObject);
 
-        //    return characterComponent;
-        //}
-
-        return null;
-    }
-
-    private static GameObject CreateCharacterObject(string name, Vector3 origin)
-    {
-        GameObject characterObject = new GameObject(name);
-        characterObject.transform.localPosition = Vector3.zero;
-        characterObject.transform.position = origin;
-
-        //see if there is a spawn point around here to orient them with
-        //if(tile.ContainsSpawnPoint())
-        //{
-        //    EnvironmentSpawnPoint spawnPoint = tile.GetSpawnPoint();
-
-        //    characterObject.transform.localEulerAngles = spawnPoint.transform.eulerAngles;
-        //}
-
-        return characterObject;
-    }
-
-    private static CharacterComponent AddComponentByTeam(CharacterID characterID, GameObject characterObject)
-    {
-        TeamID teamID = GetTeamByID(characterID);
-
-        switch (teamID)
-        {
-            case TeamID.Player:
-                PlayerCharacterComponent playerCharacterComponent = characterObject.AddComponent<PlayerCharacterComponent>();
-                playerCharacterComponent.SetID(characterID);
-                return playerCharacterComponent;
-            case TeamID.Enemy:
-                EnemyCharacterComponent enemyCharacterComponent = characterObject.AddComponent<EnemyCharacterComponent>();
-                enemyCharacterComponent.SetID(characterID);
-                return enemyCharacterComponent;
-            default:
-                return null;
-        }
-    }
 }
