ef878b07252c1e82c3954ac6569f8d1d38e4df48
Arash Tadjiki <atadjiki@gmail.com>
2024-01-21 21:03:06 -0600

    terrain grid system

---
+++ HeroicBloodshed/Assets/Packages/TerrainGridSystem/Demos/Demo31_TerritoriesFromInitialCells/Demo31.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace TGS {
+    public class Demo31 : MonoBehaviour {
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+
+}
