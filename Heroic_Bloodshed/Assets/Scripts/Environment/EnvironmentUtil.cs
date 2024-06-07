06173045fed9e8f40f2f128f98fa253ad5252e09
Arash Tadjiki <atadjiki@gmail.com>
2024-06-07 16:29:11 -0500

    Cleaning up tile graph code

--- Heroic_Bloodshed/Assets/Scripts/Environment/EnvironmentUtil.cs
+++ Heroic_Bloodshed/Assets/Scripts/Environment/EnvironmentUtil.cs
@@ -4,52 +4,6 @@ using Pathfinding;
 using UnityEngine;
 using static Constants;
 
-[Serializable]
-public struct TileConnectionInfo
-{
-    public EnvironmentLayer Layer;
-    public EnvironmentLayer Obstruction;
-
-    public static TileConnectionInfo Build()
-    {
-        return new TileConnectionInfo()
-        {
-            Layer = EnvironmentLayer.NONE,
-            Obstruction = EnvironmentLayer.NONE,
-        };
-    }
-
-    public bool IsWallBetween()
-    {
-        return Obstruction == EnvironmentLayer.WALL;
-    }
-
-    public bool IsObstructed()
-    {
-        return Obstruction != EnvironmentLayer.NONE;
-    }
-
-    public bool IsUnobstructed()
-    {
-        return (IsObstructed() == false);
-    }
-
-    public bool IsValid()
-    {
-        return IsLayerTraversible(Layer) && Obstruction == EnvironmentLayer.NONE;
-    }
-
-    public bool IsInvalid()
-    {
-        return (IsValid() == false);
-    }
-
-    public bool ProvidesCover()
-    {
-        return IsLayerObstacle(Layer) && IsLayerCover(Obstruction);
-    }
-}
-
 public class EnvironmentUtil
 {
     public static EnvironmentLayer CheckTileLayer(Vector3 origin)
---
+++ Heroic_Bloodshed/Assets/Scripts/Environment/TileConnectionInfo.cs
@@ -0,0 +1,48 @@
+using System;
+using static Constants;
+
+[Serializable]
+public struct TileConnectionInfo
+{
+    public EnvironmentLayer Layer;
+    public EnvironmentLayer Obstruction;
+
+    public static TileConnectionInfo Build()
+    {
+        return new TileConnectionInfo()
+        {
+            Layer = EnvironmentLayer.NONE,
+            Obstruction = EnvironmentLayer.NONE,
+        };
+    }
+
+    public bool IsWallBetween()
+    {
+        return Obstruction == EnvironmentLayer.WALL;
+    }
+
+    public bool IsObstructed()
+    {
+        return Obstruction != EnvironmentLayer.NONE;
+    }
+
+    public bool IsUnobstructed()
+    {
+        return (IsObstructed() == false);
+    }
+
+    public bool IsValid()
+    {
+        return IsLayerTraversible(Layer) && Obstruction == EnvironmentLayer.NONE;
+    }
+
+    public bool IsInvalid()
+    {
+        return (IsValid() == false);
+    }
+
+    public bool ProvidesCover()
+    {
+        return IsLayerObstacle(Layer) && IsLayerCover(Obstruction);
+    }
+}
